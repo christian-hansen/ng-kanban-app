9f7878d6c631cb41c2c2506bd30374bc27fef136
Christian Hansen <mail@hansen-net.de>
2024-08-07 15:02:38 +0200

    Register form incl. validation

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators} from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { PasswordModule } from 'primeng/password';
@@ -11,7 +11,7 @@ import { Message } from 'primeng/api';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule, InputTextModule, ButtonModule, PasswordModule, MessagesModule],
+  imports: [FormsModule, InputTextModule, ButtonModule, PasswordModule, MessagesModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
@@ -20,48 +20,54 @@ export class RegisterComponent {
   first_name: string = '';
   last_name: string = '';
   password: string = '';
-  password2: string = ''
+  password_repeat: string = ''
   email: string = '';
   formdisabled: boolean = false;
   public error: string = '';
   messages: Message[] = [];
 
+  regForm = new FormGroup({
+    username: new FormControl('', [Validators.minLength(2), Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    first_name: new FormControl('', [Validators.minLength(2),Validators.required]),
+    last_name: new FormControl('', [Validators.minLength(2), Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    password_repeat: new FormControl('', [Validators.required]),
+  });
+
   constructor(private auth: AuthService, private router: Router, private cd:ChangeDetectorRef) {}
-
-
-  // ngOnInit() : void {
-  //   this.messages = [{ severity: 'error', detail: `Error message` }];
-  // }
-
 
   async register() {
     this.formdisabled = true;
-    let data = {
-      username: this.username,
-      email: this.email,
-      password: this.password,
-      first_name: this.first_name,
-      last_name: this.last_name
+    let formData = {
+      username: this.regForm.value.username,
+      email: this.regForm.value.email,
+      password: this.regForm.value.password,
+      first_name: this.regForm.value.first_name,
+      last_name: this.regForm.value.last_name
     };
 
-    try {
-      let resp: any = await this.auth.registerWithUsernameAndPassword(
-        this.username,
-        this.email,
-        this.password,
-        this.first_name,
-        this.last_name
-      );
-      console.log(resp.message);
-      this.router.navigate(['/login']); // Navigate to /login page after successful registration
-    } catch (e) {
-      this.error = "Registration failed. Please try again"
-      this.messages = [{ severity: 'error', detail: `${this.error}` }];
-      console.error(e);
-      this.resetForm();
+    if (this.isPassWordsMatching()) {
+
+      try {        
+        let resp: any = await this.auth.registerWithUsernameAndPassword(
+          formData.username!,
+          formData.email!,
+          formData.password!,
+          formData.first_name!,
+          formData.last_name!
+        );
+        // console.log(resp.message);
+        this.router.navigate(['/login']); // Navigate to /login page after successful registration
+      } catch (e: any) {
+        this.messages = [{ severity: 'error', detail: `${e.error.error}` }];
+        console.error(e);
+        this.resetForm();
+      }
+    } else {
+      this.messages = [{ severity: 'error', detail: `Passwords are not matching` }];
     }
   }
-
 
   ngAfterViewChecked(): void {
     this.cd.detectChanges();
@@ -73,4 +79,9 @@ export class RegisterComponent {
     this.email = '';
     this.password = '';
   }
+
+  isPassWordsMatching(): boolean {
+    return this.regForm.value.password === this.regForm.value.password_repeat
+  }
+
 }
